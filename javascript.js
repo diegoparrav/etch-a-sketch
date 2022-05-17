cbdeff27aa0b5d795decc97d33234fb744f40c5b
diegoparrav <diego.parra.velasco@gmail.com>
2022-05-16 23:22:40 -0500

    added a button to the html file added margin to the button and to the container div in css modified the js file, right now it does not call the function that creates the grid, the prompt for asking the user for a valid imput is complete

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ function addDivs(){
     for (let j=1; j < 17; j++){
         for (let i = 1; i < 17; i++){
             let newDiv = document.createElement('div');
-            newDiv.classList.add('gridElement', (`item${i}`))
+            newDiv.classList.add('gridElement')
             newDiv.setAttribute('grid-column-start', `${i}`)
             newDiv.setAttribute('grid-column-end', `${i+1}`)
             newDiv.setAttribute('grid-row-start',`${j}`)
@@ -14,4 +14,22 @@ function addDivs(){
             
     }
 }
-addDivs();
+//addDivs();
+
+let boolController = true;
+
+const gridButton = document.querySelector('.button');
+gridButton.addEventListener('click', () => {
+        boolController = true;
+        while (boolController) {
+            let sideSquare = prompt('Select the size of the grid, maximum 100');
+            sideSquare = +sideSquare;
+            if ((sideSquare > 100) || (isNaN(sideSquare))) {
+                alert('Please pick a number between 1 and 100');
+                continue;
+            } else {
+                alert(`you have choosen a ${sideSquare}x${sideSquare} grid`);
+                boolController = false;
+            }
+        }
+    })
